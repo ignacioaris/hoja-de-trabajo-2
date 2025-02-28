81110ba3c1c3a25909047eb96f3c120e27a300f1
ignacioaris <arisignacio90@gmail.com>
2025-02-28 07:04:29 -0600

    Update InfixToPostfixConverter.java

--- InfixToPostfixConverter.java
+++ InfixToPostfixConverter.java
@@ -2,6 +2,69 @@ import java.util.Stack;
 
 public class InfixToPostfixConverter {
 
-public String convert(String infix) throws Exception {}
+public String convert(String infix) throws Exception {
+  StringBuilder postfix = new StringBuilder();
+  Stack<Character> stack = new Stack<>();
+
+  for (int i = 0; i < infix.length(); i++) {
+            char c = infix.charAt(i);
+
+         
+            if (c == ' ') {
+                continue;
+            }
+
+            
+            if (Character.isDigit(c)) {
+               
+                while (i < infix.length() && Character.isDigit(infix.charAt(i))) {
+                    postfix.append(infix.charAt(i));
+                    i++;
+                }
+                i--; 
+                postfix.append(' '); 
+            }
+           
+            else if (c == '(') {
+                stack.push(c);
+            }
+           
+            else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    postfix.append(stack.pop()).append(' '); 
+                }
+                if (!stack.isEmpty() && stack.peek() != '(') {
+                    throw new Exception("Expresión inválida: paréntesis no balanceados");
+                } else {
+                    stack.pop(); 
+                }
+            }
+
+            else if (isOperator(c)) {
+                while (!stack.isEmpty() && precedence(c) <= precedence(stack.peek())) {
+                    postfix.append(stack.pop()).append(' '); 
+                }
+                stack.push(c);
+            } else {
+                throw new Exception("Carácter inválido: " + c);
+            }
+        }
+
+
+        while (!stack.isEmpty()) {
+            if (stack.peek() == '(') {
+                throw new Exception("Expresión inválida: paréntesis no balanceados");
+            }
+            postfix.append(stack.pop()).append(' '); 
+        }
+
+
+        if (postfix.length() > 0 && postfix.charAt(postfix.length() - 1) == ' ') {
+            postfix.deleteCharAt(postfix.length() - 1);
+        }
+
+        return postfix.toString();
+
+}
   
 }
